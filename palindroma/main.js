2404b91fcfcb52ff4235c6b765b5d60b7afe160e
DarioGuastella <jimmyguastella@gmail.com>
2023-11-06 15:30:47 +0100

    feat: aggiunge controllo palindromo

--- palindroma/main.js
+++ palindroma/main.js
@@ -3,15 +3,32 @@ const parolaInserita = prompt("Inserisci una parola");
 
 
 // scompongo lettera per lettera la parola
-let element = [];
+let arrayOriginale = [];
 for (let i = 0; i < parolaInserita.length; i++) {
     letter = parolaInserita[i];
-    console.log(letter);
-    element.push(letter);
+    arrayOriginale.push(letter);
 }
+const parolaOriginale = arrayOriginale.toString();
 
 // inverto l'ordine delle lettere
 
+const lettereInvertite = arrayOriginale.toReversed();
+let parolaInvertita = lettereInvertite.toString();
+
+
 // controllo se le due parole sono uguali
 
+function confrontoParole(parola1, parola2) {
+    let risultato;
+    
+    if(parola1 == parola2) {
+        risultato = "palindroma";
+    } else {
+        risultato = "non palindroma";
+    }
+    
+    return risultato;
+}
 // do il risultato del controllo
+let risultato = confrontoParole(parolaOriginale, parolaInvertita);
+console.log("la tua parola è " + risultato);
