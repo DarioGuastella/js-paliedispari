cc4e5edfb336e2b74395df8fbc83684e3c8ce7e9
DarioGuastella <jimmyguastella@gmail.com>
2023-11-06 16:59:57 +0100

    aggiunge console.logs a funzione

--- pari&dispari/main.js
+++ pari&dispari/main.js
@@ -2,9 +2,12 @@ const sceltaUtente = prompt("Pari o dispari?").toLowerCase();
 const numeroUtente = Number(prompt("Scrivi un nome da 1 a 5"));
 const numeroPc = Math.floor(Math.random() * 5) + 1;
 const somma = numeroUtente + numeroPc;
-console.log(`Hai scelto: ${sceltaUtente}`)
-console.log(`Hai scelto: ${numeroUtente}`)
-console.log(`Il computer ha scelto: ${numeroPc}`)
+function gameLogs() {
+    console.log(`Hai scelto: ${sceltaUtente}`);
+    console.log(`Hai scelto: ${numeroUtente}`);
+    console.log(`Il computer ha scelto: ${numeroPc}`);
+    console.log(`La somma dei numeri è ${somma}, quindi ${risultato}`);
+}
 
 function pariOrDispari(numero) {
     let risultato;
@@ -19,7 +22,7 @@ function pariOrDispari(numero) {
 }
 
 let risultato = pariOrDispari(somma);
-console.log(`La somma dei numeri è ${somma}, quindi ${risultato}`);
+
 
 if (sceltaUtente !== "pari" && sceltaUtente !== "dispari") {
     console.log("Controlla di aver inserito correttamente i dati (pari o dispari)");
@@ -28,10 +31,13 @@ if (sceltaUtente !== "pari" && sceltaUtente !== "dispari") {
     console.log("Controlla di aver inserito correttamente i dati (numero compreso tra 1 e 5)");
 
 } else if (risultato == "pari" && sceltaUtente == "pari") {
+    gameLogs();
     console.log("hai vinto");
 } else if (risultato == "dispari" && sceltaUtente == "dispari"){
+    gameLogs();
     console.log("hai vinto");
 }  else {
+    gameLogs();
     console.log("hai perso");
 }
 
