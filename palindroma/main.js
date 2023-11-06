3bae672e6fd5f44301c51024e0e0bd440a5f7037
DarioGuastella <jimmyguastella@gmail.com>
2023-11-06 14:48:51 +0100

    inizia programma palindroma

---
+++ palindroma/main.js
@@ -0,0 +1,17 @@
+// faccio inserire una parola
+const parolaInserita = prompt("Inserisci una parola");
+
+
+// scompongo lettera per lettera la parola
+let element = [];
+for (let i = 0; i < parolaInserita.length; i++) {
+    letter = parolaInserita[i];
+    console.log(letter);
+    element.push(letter);
+}
+
+// inverto l'ordine delle lettere
+
+// controllo se le due parole sono uguali
+
+// do il risultato del controllo
